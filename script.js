a41fd8237b3b7a34907f77f5c514514137f01d8b
JonathanLundquist <76451692+JonathanLundquist@users.noreply.github.com>
2021-01-27 14:52:44 -0700

    small changes

---
+++ script.js
@@ -0,0 +1,9 @@
+//Gets the current date
+var currentDate = new Date();
+document.getElementById("currentDate").innerHTML = currentDate;
+
+//Gets the current year
+var thisYear = new Date().getFullYear();
+document.getElementById("currentYear").innerHTML = thisYear;
+
+//Calculates my age dynamically

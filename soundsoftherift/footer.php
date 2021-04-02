7bae8126a0a645fe2b1fb44d559c21ebe0c58041
Jonathan Lundquist <Jonathanlundquist97@gmail.com>
2021-04-02 12:21:52 -0700

    Changed to HTML

--- soundsoftherift/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-    <p id="copyright"> &#169; <span id="currentYear"></span> | Jonathan Lundquist | Wyoming | <a href="https://www.lundquists.com" target="_blank" rel="noopener">Project Site</a><br>
-    All audio and images are believed to be used in fair use under section 107, of the Copyright Act of 1967.</p>
-</footer>

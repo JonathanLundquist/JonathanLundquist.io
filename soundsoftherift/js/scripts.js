e2ce34181fbecf90f09c371c5fba1ba60d78f068
Jonathan Lundquist <Jonathanlundquist97@gmail.com>
2021-04-02 20:39:53 -0700

    Made Backgrounds Switch On Load

--- soundsoftherift/js/scripts.js
+++ soundsoftherift/js/scripts.js
@@ -1,5 +1,4 @@
-//Handles fetching the character Json, loading the audio, and creation the transition effects
-
+//Handles fetching the character Json, and loading the proper audio
 const url = `characters.json`;
 
 function getAudio() {
@@ -12,6 +11,8 @@ function getAudio() {
         if (!val) return;
 
         val--;
+        //Changes the background image when a new champion is selected
+        document.getElementById('theBody').style.backgroundImage = 'url("images/' + data[val].image + '")';
         for (var i=1; i < 9; i++) {
             document.getElementById("audio"+i).src = "sounds/" + eval(`data[${val}].audio${i}`);
         }
@@ -19,6 +20,7 @@ function getAudio() {
     .catch(err => alert(err));
 }
 
+//Handles dynamically building the champion selection
 function loadSelection() {
     var elem = document.getElementById('sel');
 
@@ -32,27 +34,38 @@ function loadSelection() {
     .catch(err => alert(err));
 }
 
+//Removes the playing CSS from the button
 function removeTransition(elem) {
     if (elem.propertyName !== 'transform') return;
     elem.target.classList.remove('playing');
 }
 
+
+//Dynamically get the sound based on the pressed key, and play the proper sound
 function playAudio(e) {
     const audio = document.querySelector("audio[data-key='"+e.keyCode+"']");
     const key = document.querySelector("div[data-key='"+e.keyCode+"']");
 
     if (!audio) return;
 
+    //Adds the playing CSS to a button
     key.classList.add('playing');
+    //Stops audio from playing over itself if the same sound is selected twice
     audio.currentTime = 0;
     audio.play();
 }
 
+
+//Forces the list to build on windows load
 window.onload = loadSelection();
+//Event listener for key press
 window.addEventListener('keydown', playAudio);
+//Changes the loaded audio to the proper champion when the selection is changed
 document.getElementById("sel").addEventListener('change', getAudio);
 const keys = Array.from(document.querySelectorAll('.key'));
+//Calls the remove transition function on transitioned event
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+//Allows the KBD's to be clicked as well and still function
 keys.forEach(key => key.addEventListener('click', function(e) { document.querySelector("audio[data-key='"+this.dataset.key+"']").play(); }));
 
 //handles getting the date for the footer
